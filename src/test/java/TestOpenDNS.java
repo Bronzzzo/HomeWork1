d0b944601294a9ee1847490af8b0bf798226826b
Bronzzzo <ciaoall@yandex.ru>
2021-07-29 20:16:04 +0300

    for hw2

--- src/test/java/TestOpenDNS.java
+++ src/test/java/TestOpenDNS.java
@@ -3,7 +3,12 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 
 public class TestOpenDNS {
@@ -11,20 +16,37 @@ public class TestOpenDNS {
     private Logger logger = LogManager.getLogger(TestOpenDNS.class);
     // Читаем передаваемый параметр browser (-Dbrowser)
     String env = System.getProperty("browser", "chrome");
+    // - Параметр -Doption=Имя_Опции для стратегии загрузки
+    String option = System.getProperty("strategy", "normal");
 
+    /*
+    Варианты команд для запуска
+    mvn clean test -Dbrowser=FiReFoX
+    mvn clean test -Dbrowser=chrome
+    mvn clean test -Doption='normal'
+    mvn clean test -Doption=none
+*/
 
     @BeforeEach
     public void setUp() {
-        logger.info("env = " + env);
-        driver = WebDriverFactory.getDriver(env.toLowerCase());
+        logger.info("env -> " + env);
+        logger.info("strategy -> " + option);
+        driver = WebDriverFactory.getDriver(env.toLowerCase(), option.toLowerCase());
         logger.info("Start driver!");
+
     }
 
     @Test
     public void openPage() {
+        // переход по ссылке
         driver.get("https://www.dns-shop.ru/");
-        logger.info("Открыта страница DNS - " + "https://www.dns-shop.ru/");
 
+
+
+        logger.info("Открыта страница DNS - " + "https://www.dns-shop.ru/");
+        //ожидания
+        WebElement element = driver.findElement(By.xpath("//a[@class='btn btn-additional']"));
+        element.click();
         // Вывод заголовка страницы
         String title = driver.getTitle();
         logger.info("title - " + title.toString());
@@ -33,6 +55,33 @@ public class TestOpenDNS {
         String currentUrl = driver.getCurrentUrl();
         logger.info("current URL - " + currentUrl.toString());
 
+
+        WebElement technique = driver.findElement(By.xpath("//a[@href='/catalog/17a8e9b716404e77/bytovaya-texnika/']"));
+        technique.click();
+
+        //список
+        List<WebElement> value = driver.findElements(By.xpath("//span[@class='subcategory__title']"));
+        for (WebElement e: value) {
+            logger.info("value ->" + e.getText());
+        }
+
+        // Создание Cookie
+        logger.info("Куки, которое добавили мы");
+        driver.manage().addCookie(new Cookie("Cookie 1", "This Is Cookie 1"));
+        Cookie cookie1  = driver.manage().getCookieNamed("Cookie 1");
+        logger.info(String.format("Domain: %s", cookie1.getDomain()));
+        logger.info(String.format("Expiry: %s",cookie1.getExpiry()));
+        logger.info(String.format("Name: %s",cookie1.getName()));
+        logger.info(String.format("Path: %s",cookie1.getPath()));
+        logger.info(String.format("Value: %s",cookie1.getValue()));
+        logger.info("--------------------------------------");
+
+// Добавляем задержку sleep
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
--- src/test/java/WebDriverFactory.java
+++ src/test/java/WebDriverFactory.java
@@ -1,38 +1,66 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.CapabilityType;
 
 public class WebDriverFactory {
     private static Logger logger = LogManager.getLogger(WebDriverFactory.class);
 
-    public static WebDriver getDriver(String browserName) {
+    public static WebDriver getDriver(String browserName, String strategyName) {
         switch (browserName) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-fullscreen");
+                options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
+                options.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, true);
+                options.addArguments("--incognito");
+                switch (strategyName) {
+                    case "normal":
+                        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                        break;
+                    case "eager":
+                        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+                        break;
+                    case "none":
+                        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+
+                        return new ChromeDriver(options);
+                }
                 logger.info("Драйвер для браузера Google Chrome");
-                return new ChromeDriver();
-            case "firefox" :
+                return new ChromeDriver(options);
+
+            case "firefox":
                 WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--kiosk");
+                firefoxOptions.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
+                firefoxOptions.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, true);
+                firefoxOptions.addArguments("--private");
+                switch (strategyName) {
+                    case "normal":
+                        firefoxOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                        break;
+                    case "eager":
+                        firefoxOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+                        break;
+                    case "none":
+                        firefoxOptions.setPageLoadStrategy(PageLoadStrategy.NONE);
+                        return new FirefoxDriver(firefoxOptions);
+                }
                 logger.info("Драйвер для браузера Mozilla Firefox");
-                return new FirefoxDriver();
-            case "edge" :
-                WebDriverManager.edgedriver().setup();
-                logger.info("Драйвер для браузера Microsoft Edge");
-                return new EdgeDriver();
-            case "explorer" :
-                WebDriverManager.iedriver().setup();
-                logger.info("Драйвер для браузера Microsoft Internet Explorer");
-                return new InternetExplorerDriver();
-            case "opera" :
-                WebDriverManager.operadriver().setup();
-                logger.info("Драйвер для браузера Opera");
-                return new OperaDriver();
+                return new FirefoxDriver(firefoxOptions);
+
 
             default:
                 throw new RuntimeException("Incorrect browser name");
